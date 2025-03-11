8b4936252b43fe92a89a902f13a4b412809ba182
mhbabon <mhbabon.bd@gmail.com>
2025-03-11 08:51:17 +0600

    Code Refactor

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -40,6 +40,5 @@ class TAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
 }
