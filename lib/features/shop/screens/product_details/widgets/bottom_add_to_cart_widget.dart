6830a8ec267c6f9db8e4a631ade471e24e396427
Mehedi Hasan <mhbabon.bd@gmail.com>
2025-04-13 08:18:32 +0600

    Categories
    Product Card Section
    
    Its a product card design by using custom widgets and new method Read More.

---
+++ lib/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:grocify/common/widgets/icons/t_circular_icon.dart';
+import 'package:grocify/utils/constants/colors.dart';
+import 'package:grocify/utils/constants/sizes.dart';
+import 'package:grocify/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TBottomAddToCart extends StatelessWidget {
+  const TBottomAddToCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: TSizes.defaultSpace,
+        vertical: TSizes.defaultSpace,
+      ),
+      decoration: BoxDecoration(
+          color: dark ? TColors.darkerGrey : TColors.light,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(TSizes.cardRadiusLg),
+            topRight: Radius.circular(TSizes.cardRadiusLg),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              TCircularIcon(
+                icon: Iconsax.minus,
+                backgroundColor: TColors.darkGrey,
+                width: 40,
+                height: 40,
+                color: TColors.white,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+              Text(
+                '2',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+              TCircularIcon(
+                icon: Iconsax.add,
+                backgroundColor: TColors.darkGrey,
+                width: 40,
+                height: 40,
+                color: TColors.white,
+              ),
+            ],
+          ),
+          ElevatedButton(onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(TSizes.md),
+                backgroundColor: TColors.black,
+                side: BorderSide(color: TColors.black)
+              ),
+              child: Text('Add to Cart'))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/shop/screens/product_details/widgets/product_detail_image_slider.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:grocify/common/widgets/appbar/appbar.dart';
+import 'package:grocify/common/widgets/custom_shapes/curved_edges/curved_edgs_widget.dart';
+import 'package:grocify/common/widgets/icons/t_circular_icon.dart';
+import 'package:grocify/common/widgets/images/t_rounded_image.dart';
+import 'package:grocify/utils/constants/colors.dart';
+import 'package:grocify/utils/constants/image_strings.dart';
+import 'package:grocify/utils/constants/sizes.dart';
+import 'package:grocify/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
+
+
+class TProductImageSlider extends StatelessWidget {
+  const TProductImageSlider({
+    super.key,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: dark ? TColors.darkerGrey : TColors.light,
+        child: Stack(
+          children: [
+
+            /// Main Large Image
+
+            SizedBox(height: 400, child: Padding(
+              padding: const EdgeInsets.all(TSizes.productImageRadius * 2),
+              child: Center(
+                  child: Image(image: AssetImage(TImages.productImage1),)),
+            )
+            ),
+
+            /// --- Image Slider
+            Positioned(
+              left: 10,
+              right: 0,
+              bottom: 30,
+              child: SizedBox(
+                height: 80,
+                child: ListView.separated(
+                  itemCount: 6,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  separatorBuilder: (_, __) => const SizedBox(width
+                      : TSizes.spaceBtwItems,),itemBuilder: (_, index) =>
+                    TRoundedImage(
+                        width: 80,
+                        backgroundColor: dark ? TColors.dark : TColors.white,
+                        border: Border.all(color: TColors.primary),
+                        padding: const EdgeInsets.all(TSizes.sm),
+                        imageurl: TImages.productImage1
+                    ),
+                ),
+              ),
+            ),
+
+            /// -- AppBar Icon
+            TAppBar(
+              showBackArrow: true,
+              actions: [
+                TCircularIcon(icon: Iconsax.heart5, color: Colors.red,)
+              ],
+
+            )
+          ],
+        ),
+      ) ,
+    );
+  }
+}
