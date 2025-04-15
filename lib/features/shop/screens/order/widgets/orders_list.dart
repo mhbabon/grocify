dee145a179cac10f18cc4e914ac276c71d5a746e
Mehedi Hasan <mhbabon.bd@gmail.com>
2025-04-15 23:42:29 +0600

    Categories Order Section

--- lib/features/shop/screens/order/widgets/orders_list.dart
+++ lib/features/shop/screens/order/widgets/orders_list.dart
@@ -11,26 +11,116 @@ class TOrderListItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return TRoundedContainer(
-      showBorder: true,
-      backgroundColor: dark ? TColors.dark : TColors.light,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(Iconsax.ship),
-              SizedBox(width: TSizes.spaceBtwItems / 2,),
+    return ListView.separated(
+      separatorBuilder: (_, index) => SizedBox(
+        height: TSizes.spaceBtwItems,
+      ),
+      itemCount: 10,
+      shrinkWrap: true,
+      itemBuilder: (_, index) => TRoundedContainer(
+        showBorder: true,
+        padding: EdgeInsets.all(TSizes.md),
+        backgroundColor: dark ? TColors.dark : TColors.light,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            /// --- Row 1
+            Row(
+              children: [
+                Icon(Iconsax.ship),
+                SizedBox(
+                  width: TSizes.spaceBtwItems / 2,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Processing',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .apply(color: TColors.primary, fontWeightDelta: 1),
+                      ),
+                      Text(
+                        '07 Apr 2025',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Iconsax.arrow_right_34,
+                      size: TSizes.iconSm,
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
 
+            /// --- Row 2
+            Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Icon(Iconsax.tag),
+                      SizedBox(
+                        width: TSizes.spaceBtwItems / 2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Order',
+                                style: Theme.of(context).textTheme.labelMedium),
+                            Text(
+                              '[#123ab]',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Icon(Iconsax.calendar),
+                      SizedBox(
+                        width: TSizes.spaceBtwItems / 2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Shipping Date',
+                              style: Theme.of(context).textTheme.labelMedium,
+                            ),
+                            Text(
+                              '07 May 2025',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
 
-              Column(
-                children: [
-                  Text('Processing',style: Theme.of(context).textTheme.bodyLarge!.apply(color: TColors.primary,fontWeightDelta: 1),),
-                  Text('07 Apr 2025', style: Theme.of(context).textTheme.headlineSmall,),
-                ],
-              )
-            ],
-          )
-        ],
+            /// --- Row 2
+          ],
+        ),
       ),
     );
   }
