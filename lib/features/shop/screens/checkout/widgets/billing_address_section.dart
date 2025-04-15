1a6cd72a8eb543bdfb58752cddac3bf7c520bc3e
Mehedi Hasan <mhbabon.bd@gmail.com>
2025-04-15 09:21:20 +0600

    Categories Checkout Section
    Checkout  section is design by custom widget and if user wants they can bbe change.

---
+++ lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:grocify/common/widgets/texts/section_heading.dart';
+import 'package:grocify/utils/constants/sizes.dart';
+
+class TBillingAddressSection extends StatelessWidget {
+  const TBillingAddressSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TSectionHeading(
+          title: 'Shipping Address',
+          buttonTitle: 'Change',
+          onPressed: () {},
+        ),
+        Text(
+          'Dart Dare Devils',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.phone,
+              color: Colors.grey,
+              size: 16,
+            ),
+            SizedBox(
+              width: TSizes.spaceBtwItems,
+            ),
+            Text(
+              '(+(880)-1712-004187',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwItems / 2,
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.location_history,
+              color: Colors.grey,
+              size: 16,
+            ),
+            SizedBox(
+              width: TSizes.spaceBtwItems,
+            ),
+            Text(
+              '411621/1, Dhanmondi, Dhaka, Bangladesh',
+              style: Theme.of(context).textTheme.bodyMedium,
+              softWrap: true,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
